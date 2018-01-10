1b073deb5832095fe4b3551770964934e2e481eb
Mike Richards <mjrichards91@gmail.com>
2018-01-10 16:22:59 -0500

    Completed tutorial code

---
+++ app/views/list/list.js
@@ -0,0 +1,78 @@
+var dialogsModule = require("ui/dialogs");
+var observableModule = require('data/observable')
+var GroceryListViewModel = require("../../shared/view-models/grocery-list-view-model");
+var socialShare = require('nativescript-social-share');
+var swipeDelete = require('../../shared/utils/ios-swipe-delete');
+
+var page;
+
+var groceryList = new GroceryListViewModel([]);
+var pageData = new observableModule.fromObject({
+    groceryList: groceryList,
+    grocery: ""
+});
+
+exports.loaded = function (args) {
+    page = args.object;
+    page.bindingContext = pageData;
+
+    if (page.ios) {
+        var listView = page.getViewById("groceryList");
+        swipeDelete.enable(listView, function (index) {
+            groceryList.delete(index);
+        });
+    }
+
+    var listView = page.getViewById('groceryList')
+
+    groceryList.empty();
+
+    pageData.set("isLoading", true);
+    groceryList.load().then(function () {
+        pageData.set("isLoading", false);
+
+        listView.animate({
+            opacity: 1,
+            duration: 1000
+        });
+    });
+};
+
+exports.add = function () {
+    // Check for empty submissions
+    if (pageData.get("grocery").trim() === "") {
+        dialogsModule.alert({
+            message: "Enter a grocery item",
+            okButtonText: "OK"
+        });
+        return;
+    }
+
+    // Dismiss the keyboard
+    page.getViewById("grocery").dismissSoftInput();
+    groceryList.add(pageData.get("grocery"))
+        .catch(function () {
+            dialogsModule.alert({
+                message: "An error occurred while adding an item to your list.",
+                okButtonText: "OK"
+            });
+        });
+
+    // Empty the input field
+    pageData.set("grocery", "");
+};
+
+exports.share = function () {
+    var list = [];
+    for (var i = 0, size = groceryList.length; i < size; i++) {
+        list.push(groceryList.getItem(i).name);
+    }
+    var listString = list.join(", ").trim();
+    socialShare.shareText(listString);
+};
+
+exports.delete = function (args) {
+    var item = args.view.bindingContext;
+    var index = groceryList.indexOf(item);
+    groceryList.delete(index);
+};
---
+++ platforms/ios/build/Groceries.build/Debug-iphonesimulator/Groceries.build/Objects-normal/x86_64/main.d
@@ -0,0 +1,11 @@
+dependencies: \
+  /Users/mrichards/Documents/Personal/Groceries/platforms/ios/Groceries/Groceries-Prefix.pch \
+  /Users/mrichards/Documents/Personal/Groceries/platforms/ios/internal/main.m \
+  /Users/mrichards/Documents/Personal/Groceries/platforms/ios/internal/NativeScript.framework/Headers/NativeScript.h \
+  /Users/mrichards/Documents/Personal/Groceries/platforms/ios/internal/NativeScript.framework/Headers/TNSRuntimeInstrumentation.h \
+  /Users/mrichards/Documents/Personal/Groceries/platforms/ios/internal/NativeScript.framework/Headers/TNSRuntime.h \
+  /Users/mrichards/Documents/Personal/Groceries/platforms/ios/internal/NativeScript.framework/Headers/TNSRuntime+Diagnostics.h \
+  /Users/mrichards/Documents/Personal/Groceries/platforms/ios/internal/NativeScript.framework/Headers/TNSRuntime+Inspector.h \
+  /Users/mrichards/Documents/Personal/Groceries/platforms/ios/internal/TNSExceptionHandler.h \
+  /Users/mrichards/Documents/Personal/Groceries/platforms/ios/internal/TNSDebugging.h \
+  /Users/mrichards/Documents/Personal/Groceries/platforms/ios/internal/TKLiveSync/include/TKLiveSync.h
